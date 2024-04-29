fa9d7a863446620982769abe04bd85e8886565c1
Paul Müller <dev@craban.de>
2024-04-29 21:46:04 +0200

    ci: fake-release via tag

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@ set -e
 set -x
 
 
-# override version
-cd project
-git tag -a $RELEASE_VERSION -m "RC Local Analysis deploy"
-cd ..
+# override version from git tags
+rm -rf project/.git
+cp -R .git project/
 
 # install package
 python3 -m pip install ./project
