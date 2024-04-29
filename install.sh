5b7a00a76d44bb94937aafe41ca960156b1e9f46
Paul Müller <dev@craban.de>
2024-04-29 22:19:48 +0200

    ci: fake-release via tag

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ set -x
 
 # override version from git tags
 cd project
-git tag -d $RELEASE_VERSION
 git commit -a -m "RC Local Analysis release"
 git tag -a $RELEASE_VERSION -m "RC Local Analysis release"
 cd ..
